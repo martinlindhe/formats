b9ec0593f69a1f10887f8feb1b07b9eaefd47317
Martin Lindhe <martin-commit@ubique.se>
2016-03-17 17:07:38 +0100

    wip

--- cmd/formats/main.go
+++ cmd/formats/main.go
@@ -5,8 +5,8 @@ import (
 	"io"
 	"os"
 
+	ui "github.com/gizak/termui"
 	"github.com/martinlindhe/formats"
-
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -27,6 +27,33 @@ func main() {
 
 	file, _ := os.Open(*inFile)
 	defer file.Close()
+
+	err := ui.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer ui.Close()
+
+	p := ui.NewPar(":PRESS q TO QUIT DEMO")
+	p.Height = 3
+	p.Width = 50
+	p.TextFgColor = ui.ColorWhite
+	p.BorderLabel = "Text Box"
+	p.BorderFg = ui.ColorCyan
+
+	g := ui.NewGauge()
+	g.Percent = 50
+	g.Width = 50
+	g.Height = 3
+	g.Y = 11
+	g.BorderLabel = "Gauge"
+	g.BarColor = ui.ColorRed
+	g.BorderFg = ui.ColorWhite
+	g.BorderLabelFg = ui.ColorCyan
+
+	ui.Render(p, g) // feel free to call Render, it's async and non-block
+
+	// ---
 
 	reader := io.Reader(file)
 

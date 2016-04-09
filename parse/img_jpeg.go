140bb0a53d155a93c89a846f36d304c3bf05e02a
Martin Lindhe <martin-commit@ubique.se>
2016-04-09 19:41:54 +0200

    jpeg: wip

--- parse/img_jpeg.go
+++ parse/img_jpeg.go
@@ -57,16 +57,18 @@ func parseJPEG(file *os.File) (*ParsedLayout, error) {
 
 	offset := int64(0)
 
-	fileHeader := Layout{
-		Offset: offset,
-		Info:   "header",
-		Type:   Group,
-	}
-
 	for {
+
+		chunk := Layout{
+			Offset: offset,
+			Type:   Group,
+		}
+
 		fmt.Printf("Reading at %04x\n", offset)
 		magic, _ := readUint8(file, offset)
 		marker, _ := readUint8(file, offset+1)
+
+		chunk.Info = jpegChunkTypes[marker]
 
 		if magic != 0xff {
 			fmt.Printf("jpeg parse error, found %02x\n", marker)
@@ -75,144 +77,92 @@ func parseJPEG(file *os.File) (*ParsedLayout, error) {
 
 		if marker == 0xd8 {
 			// NOTE: this marker dont have any content
-			fileHeader.Childs = append(fileHeader.Childs, []Layout{
+			chunk.Childs = []Layout{
 				Layout{Offset: offset, Length: 2, Info: jpegChunkTypes[0xd8], Type: Uint16le},
-			}...)
+			}
 			offset += 2
 			continue
 		}
 		if marker == 0xd9 {
-			fileHeader.Childs = append(fileHeader.Childs, []Layout{
+			chunk.Childs = []Layout{
 				Layout{Offset: offset, Length: 2, Info: jpegChunkTypes[0xd9], Type: Uint16be},
-			}...)
+			}
 			fmt.Println("Ending parser since EOI marker was detected")
 			break
 		}
 
 		chunkLen, _ := readUint16be(file, offset+2)
 		fmt.Println("adding", chunkLen)
-
-		childs := []Layout{
-			Layout{Offset: offset, Length: 2, Info: "type " + jpegChunkTypes[marker], Type: Uint16be},
+		chunk.Length = 2 + int64(chunkLen)
+		chunk.Childs = []Layout{
+			Layout{Offset: offset, Length: 2, Info: "type", Type: Uint16be},
 			Layout{Offset: offset + 2, Length: 2, Info: "length", Type: Uint16be},
 			Layout{Offset: offset + 4, Length: int64(chunkLen) - 2, Info: "data", Type: Bytes}, // XXX
 		}
 
 		offset += 2 + int64(chunkLen)
 
-		fileHeader.Childs = append(fileHeader.Childs, childs...)
+		res.Layout = append(res.Layout, chunk)
 	}
 
-	res.Layout = append(res.Layout, fileHeader)
 	return &res, nil
 }
 
 /*
 
+case 0xDA:
+   marker.Text = "SOS - Start of scan";
 
-   var length = type.RelativeToBigEndian16("Length");
-   marker.Nodes.Add(length);
+   int component_count = ReadByte(data.offset);
 
-   uint lenghtValue = (uint)ReadInt16BE(length.offset);
+   var Components = length.RelativeToByte("Color components");
+   data.Nodes.Add(Components);
 
-   marker.length = 2 + lenghtValue;
-   Log("len = " + marker.length);
+   for (int i = 0; i < component_count; i++) {
+       //        For each component
+       //        An ID
+       //        An AC table # (Low Nibble)
+       //        An DC table # (High Nibble)
 
-
-   var data = length.RelativeTo("Data", lenghtValue - 2);
-   marker.Nodes.Add(data);
-
-   switch (marker1) {
-   case 0xC0:
-       marker.Text = "SOF0 - Baseline DCT";
-       break;
-   case 0xC1:
-       marker.Text = "SOF1 - Extended sequential DCT";
-       break;
-   case 0xC2:
-       marker.Text = "SOF2 - Progressive DCT";
-       break;
-   case 0xC3:
-       marker.Text = "SOF3 - Lossless (sequential)";
-       break;
-   case 0xC4:
-       marker.Text = "DHT - Huffman table";
-       break;
-
-   case 0xDA:
-       marker.Text = "SOS - Start of scan";
-
-       int component_count = ReadByte(data.offset);
-
-       var Components = length.RelativeToByte("Color components");
-       data.Nodes.Add(Components);
-
-       for (int i = 0; i < component_count; i++) {
-           //        For each component
-           //        An ID
-           //        An AC table # (Low Nibble)
-           //        An DC table # (High Nibble)
-
-           var kex = new BigEndian16BitChunk("Color");
-           kex.offset = data.offset + 1 + (i * kex.length);
-           data.Nodes.Add(kex);
-       }
-
-       var Unknown = new Chunk();
-       Unknown.offset = data.offset + 1 + (2 * component_count);
-       Unknown.length = 3;
-       Unknown.Text = "Unknown";
-       data.Nodes.Add(Unknown);
-
-       // TODO: now follows compressed image data, how to get length of data?
-
-       break;
-
-   case 0xDB:
-       marker.Text = "DQT - Quantization table";
-       break;
-
-   case 0xE0:
-       marker.Text = "APP0 - Application Use";
-
-       var Identifier = length.RelativeToZeroTerminatedString("Id String", 5);
-       marker.Nodes.Add(Identifier);
-
-       var Version = Identifier.RelativeToVersionMajorMinor16("Revision");
-       marker.Nodes.Add(Version);
-
-       // Units used for Resolution
-       var Units = Version.RelativeToByte("Units used");
-       marker.Nodes.Add(Units);
-
-       var Width = Units.RelativeToBigEndian16("Width");
-       marker.Nodes.Add(Width);
-
-       var Height = Width.RelativeToBigEndian16("Height");
-       marker.Nodes.Add(Height);
-
-       var XThumbnail = Height.RelativeToByte("Horizontal Pixels");
-       marker.Nodes.Add(XThumbnail);
-
-       var YThumbnail = XThumbnail.RelativeToByte("Vertical Pixels");
-       marker.Nodes.Add(YThumbnail);
-       break;
-
-   case 0xE1:
-       marker.Text = "APP1 - Application Use";
-       // XXXX
-       break;
-
-   case 0xFE:
-       marker.Text = "COM - Comment";
-       break;
-
-   default:
-       throw new Exception("TODO " + marker1.ToString("x2"));
+       var kex = new BigEndian16BitChunk("Color");
+       kex.offset = data.offset + 1 + (i * kex.length);
+       data.Nodes.Add(kex);
    }
 
-   BaseStream.Position += lenghtValue - 2;
-   res.Add(marker);
+   var Unknown = new Chunk();
+   Unknown.offset = data.offset + 1 + (2 * component_count);
+   Unknown.length = 3;
+   Unknown.Text = "Unknown";
+   data.Nodes.Add(Unknown);
 
+   // TODO: now follows compressed image data, how to get length of data?
+
+   break;
+
+case 0xE0:
+   marker.Text = "APP0 - Application Use";
+
+   var Identifier = length.RelativeToZeroTerminatedString("Id String", 5);
+   marker.Nodes.Add(Identifier);
+
+   var Version = Identifier.RelativeToVersionMajorMinor16("Revision");
+   marker.Nodes.Add(Version);
+
+   // Units used for Resolution
+   var Units = Version.RelativeToByte("Units used");
+   marker.Nodes.Add(Units);
+
+   var Width = Units.RelativeToBigEndian16("Width");
+   marker.Nodes.Add(Width);
+
+   var Height = Width.RelativeToBigEndian16("Height");
+   marker.Nodes.Add(Height);
+
+   var XThumbnail = Height.RelativeToByte("Horizontal Pixels");
+   marker.Nodes.Add(XThumbnail);
+
+   var YThumbnail = XThumbnail.RelativeToByte("Vertical Pixels");
+   marker.Nodes.Add(YThumbnail);
+   break;
 
 */

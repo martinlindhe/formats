3c1c59c4fb59096a42d5bcf28d1d55ee01a0ea30
Martin Lindhe <martin-commit@ubique.se>
2016-04-18 01:18:44 +0200

    parse: improve elf
    Former-commit-id: 8b851dd0668697fde040f6e97a339e032643a885

--- parse/exe_elf.go
+++ parse/exe_elf.go
@@ -1,6 +1,7 @@
 package parse
 
-// STATUS: 1%
+// Executable and Linkable Format
+// STATUS: 20%
 
 import (
 	"encoding/binary"
@@ -17,6 +18,22 @@ var (
 		1: "lsb",
 		2: "msb",
 	}
+	elfOSABIs = map[byte]string{
+		0:    "system v",
+		1:    "hp-ux",
+		2:    "netbsd",
+		3:    "linux",
+		6:    "solaris",
+		7:    "aix",
+		8:    "irix",
+		9:    "freebsd",
+		0xc:  "openbsd",
+		0xd:  "openvms",
+		0xe:  "nsk os",
+		0xf:  "aros",
+		0x10: "fenix os",
+		0x11: "cloud abi",
+	}
 	elfTypes = map[uint16]string{
 		0:      "none",
 		1:      "relocatable file",
@@ -27,14 +44,20 @@ var (
 		0xffff: "ET_HIPROC",
 	}
 	elfMachines = map[uint16]string{
-		0: "none",
-		1: "AT&T WE 32100",
-		2: "SPARC",
-		3: "intel 80386",
-		4: "motorola 68000",
-		5: "motorola 88000",
-		7: "intel 80860",
-		8: "MIPS RS3000",
+		0:    "none",
+		1:    "AT&T WE 32100",
+		2:    "SPARC",
+		3:    "intel 80386",
+		4:    "motorola 68000",
+		5:    "motorola 88000",
+		7:    "intel 80860",
+		8:    "MIPS",
+		0x14: "powerpc",
+		0x28: "arm",
+		0x2a: "superh",
+		0x32: "ia-64",
+		0x3e: "x86-64",
+		0xb7: "aarch64",
 	}
 )
 
@@ -78,6 +101,12 @@ func parseELF(file *os.File) (*ParsedLayout, error) {
 		encodingName = val
 	}
 
+	osABI, _ := readUint8(file, pos+7)
+	osABIName := "?"
+	if val, ok := elfOSABIs[osABI]; ok {
+		osABIName = val
+	}
+
 	elfType, _ := readUint16le(file, pos+16)
 	typeName := "?"
 	if val, ok := elfTypes[elfType]; ok {
@@ -110,13 +139,15 @@ func parseELF(file *os.File) (*ParsedLayout, error) {
 				{Offset: pos + 4, Length: 1, Info: "class = " + className, Type: Uint8},
 				{Offset: pos + 5, Length: 1, Info: "data encoding = " + encodingName, Type: Uint8},
 				{Offset: pos + 6, Length: 1, Info: "header version", Type: Uint8},
-				{Offset: pos + 7, Length: 9, Info: "reserved", Type: Bytes},
+				{Offset: pos + 7, Length: 1, Info: "os abi = " + osABIName, Type: Bytes},
+				{Offset: pos + 8, Length: 1, Info: "os abi params", Type: Uint8},
+				{Offset: pos + 9, Length: 7, Info: "reserved", Type: Bytes},
 				{Offset: pos + 16, Length: 2, Info: "type = " + typeName, Type: Uint16le},
 				{Offset: pos + 18, Length: 2, Info: "machine = " + machineName, Type: Uint16le},
 				{Offset: pos + 20, Length: 4, Info: "version", Type: Uint32le},
 				{Offset: pos + 24, Length: 4, Info: "entry", Type: Uint32le},
 				{Offset: pos + 28, Length: 4, Info: "program header offset", Type: Uint32le},
-				{Offset: pos + 32, Length: 4, Info: "section header offset", Type: Uint32le},
+				{Offset: pos + 32, Length: 4, Info: "section he§ader offset", Type: Uint32le},
 				{Offset: pos + 36, Length: 4, Info: "flags", Type: Uint32le},
 				{Offset: pos + 40, Length: 2, Info: "elf header size", Type: Uint16le},
 				{Offset: pos + 42, Length: 2, Info: "program header entry size", Type: Uint16le},
@@ -129,25 +160,29 @@ func parseELF(file *os.File) (*ParsedLayout, error) {
 	if phOffset > 0 {
 		pos = int64(phOffset)
 		phLen := int64(phEntrySize * phCount)
-		res.Layout = append(res.Layout, Layout{
-			Offset: pos,
-			Length: phLen,
-			Info:   "program header",
-			Type:   Group,
-			Childs: []Layout{}, // XXX childs
-		})
+		if phLen > 0 {
+			res.Layout = append(res.Layout, Layout{
+				Offset: pos,
+				Length: phLen,
+				Info:   "program header",
+				Type:   Group,
+				Childs: []Layout{}, // XXX childs
+			})
+		}
 	}
 
 	if shOffset > 0 {
 		pos = int64(shOffset)
 		shLen := int64(shEntrySize * shCount)
-		res.Layout = append(res.Layout, Layout{
-			Offset: pos,
-			Length: shLen,
-			Info:   "section header",
-			Type:   Group,
-			Childs: []Layout{}, // XXX childs
-		})
+		if shLen > 0 {
+			res.Layout = append(res.Layout, Layout{
+				Offset: pos,
+				Length: shLen,
+				Info:   "section header",
+				Type:   Group,
+				Childs: []Layout{}, // XXX childs
+			})
+		}
 	}
 
 	return &res, nil

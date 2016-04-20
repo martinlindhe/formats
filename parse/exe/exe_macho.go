458e67836cca49689181adcd711c55f144b642e8
Martin Lindhe <martin-commit@ubique.se>
2016-04-20 16:24:53 +0200

    parse/mach-o: decode file type
    Former-commit-id: 173339de1dabcba6843337bb03a3ed19b040b6f0

--- parse/exe/exe_macho.go
+++ parse/exe/exe_macho.go
@@ -3,8 +3,9 @@ package exe
 // MacOS Mach-O executable
 // NOTE: on MacOS, there is C headers in /usr/include/mach-o
 // TODO: handle the CIGAM byte ordered files (ppc, need samples)
+// https://github.com/thetlk/Mach-O/tree/master/pymacho
 
-// STATUS: 1%
+// STATUS: 2%
 
 import (
 	"encoding/binary"
@@ -49,6 +50,19 @@ var (
 		0x1000007: "x86-64",
 		0x1000018: "powerpc64",
 	}
+	machoFileTypes = map[uint32]string{
+		1:  "object",       // relocatable object file
+		2:  "execute",      // demand paged executable file
+		3:  "fixed vm lib", // fixed VM shared library file
+		4:  "core",         // core file
+		5:  "preload",      // preloaded executable file
+		6:  "dylib",        // dynamically bound shared library
+		7:  "dylinker",     // dynamic link editor
+		8:  "bundle",       // dynamically bound bundle file
+		9:  "dylib stub",   // shared library stub for static linking only, no section contents
+		10: "dsym",         // companion file with only debug sections
+		11: "kext bundle",  // x86_64 kexts
+	}
 )
 
 func MachO(c *parse.ParseChecker) (*parse.ParsedLayout, error) {
@@ -62,9 +76,7 @@ func MachO(c *parse.ParseChecker) (*parse.ParsedLayout, error) {
 func isMachO(hdr *[0xffff]byte) bool {
 
 	b := *hdr
-
 	val := binary.LittleEndian.Uint32(b[:])
-
 	if val == MH_MAGIC || val == MH_MAGIC_64 || val == MH_CIGAM || val == MH_CIGAM_64 {
 		return true
 	}
@@ -76,6 +88,7 @@ func parseMachO(file *os.File, pl parse.ParsedLayout) (*parse.ParsedLayout, erro
 	pos := int64(0)
 	mhName, _ := parse.ReadToMap(file, parse.Uint32le, pos, machoMagicTypes)
 	cpuTypeName, _ := parse.ReadToMap(file, parse.Uint32le, pos+4, machoCpuTypes)
+	fileTypeName, _ := parse.ReadToMap(file, parse.Uint32le, pos+12, machoFileTypes)
 	pl.FormatName = "mach-o " + cpuTypeName
 	pl.FileKind = parse.Executable
 	pl.Layout = []parse.Layout{{
@@ -86,8 +99,8 @@ func parseMachO(file *os.File, pl parse.ParsedLayout) (*parse.ParsedLayout, erro
 		Childs: []parse.Layout{
 			{Offset: pos, Length: 4, Info: "magic = " + mhName, Type: parse.Uint32le},
 			{Offset: pos + 4, Length: 4, Info: "cpu type = " + cpuTypeName, Type: parse.Uint32le},
-			{Offset: pos + 8, Length: 4, Info: "cpu subtype", Type: parse.Uint32le}, // XXX map ...
-			{Offset: pos + 12, Length: 4, Info: "file type", Type: parse.Uint32le},  // XXX ?
+			{Offset: pos + 8, Length: 4, Info: "cpu subtype", Type: parse.Uint32le},
+			{Offset: pos + 12, Length: 4, Info: "file type = " + fileTypeName, Type: parse.Uint32le},
 			{Offset: pos + 16, Length: 4, Info: "n cmds", Type: parse.Uint32le},
 			{Offset: pos + 20, Length: 4, Info: "size of cmds", Type: parse.Uint32le},
 			{Offset: pos + 24, Length: 4, Info: "flags", Type: parse.Uint32le},
